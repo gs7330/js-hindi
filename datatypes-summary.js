118f940d794285b6cc4303c15f5ac852cb6c4a91
Sreeja Bhaskaran <gs7330@gmail.com>
2024-04-18 16:58:18 +1000

    modified

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -3,3 +3,13 @@
 //7 types: String ,Number,Boolean,null,undefined,Symbol(used to make it unique),BigInt
 
 //Reference types::(Non primitive. Arrays,objects**,Functions
+
+let str1 ="sreeja"
+console.log(str1)
+str1="sree"
+console.log(str1)
+//const id = Symbol('123')
+//const id1 = Symbol('123')
+
+//console.log(id===id1);
+//console.table([id,id1]);
